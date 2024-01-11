f6d22976c7340995df718ac3681892fa5e656593
necati mert erlus <mert.erlus@outlook.com>
2024-01-11 22:30:37 +0300

    Add project files.

---
+++ SignalRChatApp/Models/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace SignalRChatApp.Models
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string username, string message)
+        {
+           Clients.All.sendMessage(username, message);
+        }
+    }
+}
